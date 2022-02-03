1d919022131fe15dfaf17352cecb76b63b05406b
Zian Waqas Javed <zianjaved@hotmail.com>
2022-02-03 11:43:59 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+class Pet {
+    constructor(name){
+        this._name = name;
+        this._hunger = 100;
+        this._thirst = 100;
+        this._happy = 50;
+    }
+    
+    get name(){
+        return this._name;
+    }
+    get hunger(){
+        return this._hunger;
+    }
+    get thirst(){
+        return this._thirst;
+    }
+    increaseHunger(){
+        this._hunger--;
+    }
+    increaseThirst(){
+        this._thirst--;
+    }
+    increaseHappy(){
+        this._happy++;
+    }
+    decreaseHappy(){
+        this._happy--;
+    }
+}
+
+
+class Cat extends Pet {
+    constructor(name, lovesChicken){
+        super(name);
+        this._lovesChicken = lovesChicken;
+ 
+    }
+    get lovesChicken(){
+        return this._lovesChicken;
+    }
+
+}
+
+class Dog extends Pet {
+    constructor(name, lovesWalks){
+        super(name);
+        this._lovesWalks = lovesWalks;
+ 
+    }
+    get lovesWalks(){
+        return this._lovesWalks;
+    }
+
+}
+
+const jimmy = new Cat ("Zian", true)
+const bobby = new Dog ("Asma", true)
+
+console.log(jimmy)
+console.log(bobby)
+
+
+
+
+
+
+
+
+
+
